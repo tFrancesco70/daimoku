d009a07f55a051e20dbb718c2d4ad6c7dcbd999a
tFrancesco70 <tfrancesco70@gmail.com>
2026-06-10 16:49:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,25 @@
-const CACHE = "daimoku-v1";
-const FILES = ["./", "./manifest.json", "./Crane_192.png", "./Crane_256.png"];
-self.addEventListener("install", e => {
+const CACHE = 'daimoku-v2';
+const FILES = ['./', './manifest.json', './Crane_192.png', './Crane_256.png'];
+
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+// Network first: try network, fall back to cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
